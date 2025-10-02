946e99effe99e3346b922ac880379300caa0d1c8
kirubel <kirubelaynalem92@gmail.com>
2025-10-02 11:29:36 +0300

    Adding Trasaction list

--- expense_tracker/src/components/TransactionList.vue
+++ expense_tracker/src/components/TransactionList.vue
@@ -1,13 +1,22 @@
 <template>
   <h3>History</h3>
+
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
-    </li>
-  </ul>
-  <ul id="list" class="list">
-    <li class="plus">
-      payCheck <span>$400</span><button class="delete-btn">x</button>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
+<script setup>
+const transactions = [
+  { id: 1, text: "flower", amount: -19.99 },
+  { id: 2, text: "salary", amount: 299.97 },
+  { id: 3, text: "Book", amount: -34 },
+  { id: 4, text: "camera", amount: 140.97 },
+];
+</script>
